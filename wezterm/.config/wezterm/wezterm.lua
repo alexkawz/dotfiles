b92e4d4239e7b8f63484adb2aa4caf884d744ed5
Alex <alexkaw94@gmail.com>
2025-05-04 22:49:03 -0700

    updates

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hodl the config
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+config.font_size = 14
+
+config.enable_tab_bar = false
+
+config.window_decorations = "NONE"
+
+config.color_scheme = 'Monokai (base16)'
+
+config.window_background_opacity = 0.9
+-- keep adding config options
+
+
+return config
