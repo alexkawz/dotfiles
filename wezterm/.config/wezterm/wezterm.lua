311ed63d52f0785a624039c7579c1a762ebb3516
Alex K. <alexkaw94@gmail.com>
2025-05-12 10:52:28 -0700

    Added font fallback

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,16 +5,19 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 --Comment out for mac
-config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
-config.font_size = 14
+--config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+--config.font_size = 14
 --config.window_decorations = "NONE"
 -- Comment out for linux
 --config.font = wezterm.font("CaskaydiaMono Nerd Font")
---config.font_size = 18
---config.window_decorations = "RESIZE"
+config.font_size = 18
+config.window_decorations = "RESIZE"
 
 
-
+config.font = wezterm.font_with_fallback {
+  'CaskaydiaMono Nerd Font',
+  'CaskaydiaCove Nerd Font Mono',
+}
 
 config.enable_tab_bar = false
 
