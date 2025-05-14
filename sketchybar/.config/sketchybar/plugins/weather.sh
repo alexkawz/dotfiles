3e563e0fd9d32cfab168df9570285e737817d23b
Alex K. <alexkaw94@gmail.com>
2025-05-14 09:17:09 -0700

    Changed Location and Language

--- sketchybar/.config/sketchybar/plugins/weather.sh
+++ sketchybar/.config/sketchybar/plugins/weather.sh
@@ -4,13 +4,13 @@ sketchybar --set $NAME \
   icon.color=0xff5edaff
 
 # fetch weather data
-LOCATION="Seoul"
+LOCATION="Irvine"
 REGION=""
-LANG="ko"
+LANG="en"
 
 # Line below replaces spaces with +
 LOCATION_ESCAPED="${LOCATION// /+}+${REGION// /+}"
-WEATHER_JSON=$(curl -s "https://wttr.in/$LOCATION_ESCAPED?0pq&format=j1&lang=$LANG")
+WEATHER_JSON=$(curl -s "https://wttr.in/$LOCATION_ESCAPED?0Fpq&format=j1&lang=$LANG")
 
 # Fallback if empty
 if [ -z $WEATHER_JSON ]; then
@@ -18,9 +18,9 @@ if [ -z $WEATHER_JSON ]; then
   return
 fi
 
-TEMPERATURE=$(echo $WEATHER_JSON | jq '.current_condition[0].temp_C' | tr -d '"')
+TEMPERATURE=$(echo $WEATHER_JSON | jq '.current_condition[0].temp_F' | tr -d '"')
 #WEATHER_DESCRIPTION=$(echo $WEATHER_JSON | jq '.current_condition[0].weatherDesc[0].value' | tr -d '"' | sed 's/\(.\{16\}\).*/\1.../')
 WEATHER_DESCRIPTION=$(echo $WEATHER_JSON | jq '.current_condition[0].lang_ko[0].value' | tr -d '"' | sed 's/\(.\{16\}\).*/\1.../')
 
 sketchybar --set $NAME \
-  label="$TEMPERATURE$(echo '°')C • $WEATHER_DESCRIPTION"
+  label="$TEMPERATURE$(echo '°')F • $WEATHER_DESCRIPTION"
